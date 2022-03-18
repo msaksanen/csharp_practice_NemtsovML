1be557345293930da50a7bcbaa8bab407b554801
msaksanen <opml2007@gmail.com>
2022-03-18 21:39:06 +0300

    Task 1 Commit

---
+++ lecture_7/Csharp.Practice/7.Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _7.Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 10;
+            int?[] numr = new int?[n];  //Only nullable types. Non-nullable value types cannot be converted (see screenshot).
+            for (int i = 0; i < n; i++)
+            {
+                numr[i] = null;
+                Console.WriteLine($"Item{i} " + numr[i]);
+            }
+        }
+    }
+}
